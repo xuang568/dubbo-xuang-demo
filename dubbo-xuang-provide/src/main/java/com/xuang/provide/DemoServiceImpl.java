c5fcdec58bb7379733b25ac1ed94600c2458b669
ang <1253217959@qq.com>
2018-05-23 01:10:38 +0800

    dubbo-xuang-demo

--- dubbo-xuang-provide/src/main/java/com/xuang/provide/DemoServiceImpl.java
+++ dubbo-xuang-provide/src/main/java/com/xuang/provide/DemoServiceImpl.java
@@ -10,6 +10,7 @@ import java.util.Date;
 public class DemoServiceImpl implements DemoService {
     public String sayHello(String name) {
         System.out.println("[" + new SimpleDateFormat("HH:mm:ss").format(new Date()) + "] Hello " + name + ", request from consumer: ");
+        System.out.println("服务启动");
         return "Hello " + name + ", response from provider: ";
     }
 }
